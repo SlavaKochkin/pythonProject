549f612421d9788833504b1bf4a1c453535d7a60
SlavaKochkin <89229055025@mail.ru>
2025-09-16 14:30:06 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,97 +1,17 @@
-def load_contacts():
-    contacts = []
-    with open('data.txt', 'r', encoding='utf-8') as file:
-        for r in file:
-            r = r.strip()
-            if not r:
-                continue
-            parts = r.split('|')
+from controller import Controller
+from model import ContactBook
+from view import View
 
-            while len(parts) < 4:
-                parts.append('')
+def main():
+    model = ContactBook()
+    view = View()
+    controller = Controller(model, view)
 
-            if len(parts) >= 4:
-                parts = parts[:4]
-                contact = {
-                    'id': parts[0],
-                    'name': parts[1],
-                    'phone': parts[2],
-                    'comment': parts[3]
-                }
-                contacts.append(contact)
-    return contacts
+    controller.show_all_contacts()
+    controller.run()
 
-contacts = load_contacts()
-for contact in contacts:
-    print(f"ID: {contact['id']}, Имя: {contact['name']}, Телефон: {contact['phone']}, Комментарий: {contact['comment']}")
-
-
-
-def add_contact(id, name, phone, comment):
-    with open('data.txt', 'a', encoding='utf-8') as file:
-        line = f"{id}|{name}|{phone}|{comment}\n"
-        file.write(line)
-
-
-
-id = input("Введите ID: ")
-name = input("Введите имя: ")
-phone = input("Введите телефон:")
-comment = input("Введите комментарий: ")
-
-add_contact(id, name, phone, comment)
-
-
-def show_menu():
-    print("Выберите действие:")
-    print("1 - Показать все контакты")
-    print("2 - Добавить контакт")
-    print("0 - Выйти")
-
-def load_contacts():
-    contacts = []
-    with open('data.txt', 'r', encoding='utf-8') as file:
-        for r in file:
-            r = r.strip()
-            parts = r.split('|')
-            if len(parts) == 4:
-                contact = {
-                    'id': parts[0],
-                    'name': parts[1],
-                    'phone': parts[2],
-                    'comment': parts[3]
-                }
-                contacts.append(contact)
-    return contacts
-
-def add_contact(id, name, phone, comment):
-    with open('data.txt', 'a', encoding='utf-8') as file:
-        line = f'{id}|{name}|{phone}|{comment}\n'
-        file.write(line)
-
-while True:
-    show_menu()
-    choice = input("Введите номер действия: ")
-
-    if choice == '1':
-        contacts = load_contacts()
-        for contact in contacts:
-            print(f"ID: {contact['id']}, Имя: {contact['name']}, Телефон: {contact['phone']}, Комментарий: {contact['comment']}")
-    elif choice == '2':
-        id = input("Введите id: ")
-        name = input("Введите имя: ")
-        phone = input("Введите телефон:")
-        comment = input("Введите комментарий: ")
-        add_contact(id, name, phone, comment)
-        print("Контакт добавлен!\n")
-        contacts = load_contacts()
-        for contact in contacts:
-            print(f"ID: {contact['id']}, Имя: {contact['name']}, Телефон: {contact['phone']}, Комментарий: {contact['comment']}")
-    elif choice == "0":
-        print("Выход из программы...")
-        break
-    else:
-        print("Неверный выбор, попробуйте заново.")
+if __name__ == "__main__":
+    main()
 
 
 
